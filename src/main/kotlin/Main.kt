797d6c33b1b570fc329608dec088f177fabe9dc2
veyndan <veyndan@gmail.com>
2020-05-23 14:34:14 +0200

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>) {
+    val output = """<?xml version="1.0" encoding="utf-8"?>
+<resources>
+    <color name="colorPrimary">@android:color/white</color>
+    <color name="colorPrimaryDark">#f5f5f5</color>
+    <color name="colorAccent">#FF4081</color>
+</resources>
+"""
+    println(output)
+}
